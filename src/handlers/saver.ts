a170963f1e8c3d43abeffe2765799a2490c72d3c
gsk <dfyz0r@list.ru>
2021-02-26 14:45:01 +0300

    feat(saver.ts): add clearSaveDirectory method

--- src/handlers/saver.ts
+++ src/handlers/saver.ts
@@ -7,17 +7,19 @@ import { IWeekSchedule } from '../index.js';
 class Saver {
   private readonly dirname: string;
   private readonly saveDir: string;
+  private readonly saveDirPath: string;
 
   constructor(saveDir: string) {
     this.dirname = path.resolve();
     this.saveDir = saveDir;
+    this.saveDirPath = path.join(this.dirname, this.saveDir)
   }
 
   public async saveWeekSchedule(data: IWeekSchedule) {
     const { week, days } = data;
 
     const fileName = `schedule-week${week}.json`;
-    const savePath = path.join(this.dirname, this.saveDir, fileName);
+    const savePath = path.join(this.saveDirPath, fileName);
 
     return new Promise(((resolve, reject) => {
       fs.writeFile(savePath, JSON.stringify(days), err => {
@@ -29,6 +31,22 @@ class Saver {
         resolve(true);
       })
     }));
+  }
+
+  public clearSaveDirectory() {
+    fs.readdir(this.saveDir, (err, files) => {
+      if (err) {
+        throw err;
+      }
+
+      files.forEach(file => fs.unlink(path.join(this.saveDirPath, file), err => {
+        if (err) {
+          throw err;
+        }
+      }));
+
+      console.log(chalk.green('Директория данных очищена: ' + chalk.green.bold(this.saveDir)));
+    });
   }
 
   private static successfulSave(fileName: string) {
